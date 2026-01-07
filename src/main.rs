6fe6b89f0e9650b47cc6a61bbc210a9d4a060449
enukic <nukic.emir.1@gmail.com>
2026-01-07 23:45:29 +0100

    enable rtt logging

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,37 @@
 
 use cortex_m::delay::Delay;
 use cortex_m_rt::entry;
-use panic_halt as _;
+use defmt_rtt as _;
+use panic_probe as _;
 use stm32f7xx_hal:: {
     pac,
     prelude::*,
 };
 
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+/// Terminates the application and makes a semihosting-capable debug tool exit
+/// with status code 0.
+pub fn exit() -> ! {
+    semihosting::process::exit(0);
+}
+
+/// Hardfault handler.
+///
+/// Terminates the application and makes a semihosting-capable debug tool exit
+/// with an error. This seems better than the default, which is to spin in a
+/// loop.
+#[cortex_m_rt::exception]
+unsafe fn HardFault(_frame: &cortex_m_rt::ExceptionFrame) -> ! {
+    semihosting::process::exit(1);
+}
+
 #[entry]
 fn main() -> ! {
+    defmt::println!("ZoomPunk booting up!");
     let device_peripherals = pac::Peripherals::take().unwrap();
     let core_peripherals = cortex_m::Peripherals::take().unwrap();
     let rcc = device_peripherals.RCC.constrain();
