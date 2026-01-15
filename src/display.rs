66b5e92b5fe571cf060214b68760c8d0b9f261e3
enukic <nukic.emir.1@gmail.com>
2026-01-16 00:00:50 +0100

    refactor: move display code into the display module

--- src/display.rs
+++ src/display.rs
@@ -1,20 +1,27 @@
 use embedded_graphics::{
+    pixelcolor::raw::RawU16,
     Pixel,
     draw_target::DrawTarget,
     geometry::Size,
     pixelcolor::Rgb565,
-    pixelcolor::raw::RawU16,
     prelude::*,
 };
 
 use stm32f7xx_hal::{
-    ltdc::{DisplayConfig, DisplayController, Layer, PixelFormat, SupportedWord},
-    pac::{DMA2D, LTDC},
+    gpio::{Output, PushPull, Speed, gpioi::PI12, gpiok::PK3},
+    ltdc::{DisplayConfig, DisplayController, Layer, PixelFormat},
+    pac::{DMA2D, LTDC, GPIOE, GPIOG, GPIOH, GPIOI, GPIOJ, GPIOK},
     prelude::*,
     rcc::{HSEClock, HSEClockMode},
 };
 
-pub const DISCO_SCREEN_CONFIG: DisplayConfig = DisplayConfig {
+const WIDTH: u16 = 480;
+const HEIGHT: u16 = 272;
+
+const FB_SIZE: usize = (WIDTH as usize) * (HEIGHT as usize);
+static mut INTERNAL_FB: [u16; FB_SIZE] = [0; FB_SIZE];
+
+const DISCO_CONFIG: DisplayConfig = DisplayConfig {
     active_width: 480,
     active_height: 272,
     h_back_porch: 13,
@@ -30,25 +37,50 @@ pub const DISCO_SCREEN_CONFIG: DisplayConfig = DisplayConfig {
     pixel_clock_pol: false,
 };
 
-pub struct Stm32F7DiscoDisplay<T: 'static + SupportedWord> {
-    pub controller: DisplayController<T>,
+pub struct Display {
+    controller: DisplayController<u16>,
+    _disp_on: PI12<Output<PushPull>>,
+    _backlight: PK3<Output<PushPull>>,
 }
 
-impl <T: 'static + SupportedWord> Stm32F7DiscoDisplay<T> {
-    pub fn new(ltdc: LTDC, dma2d: DMA2D) -> Stm32F7DiscoDisplay<T> {
-        let controller = DisplayController::new(
+impl Display {
+    pub fn new(
+        ltdc: LTDC,
+        dma2d: DMA2D,
+        gpioe: GPIOE, gpiog: GPIOG, gpioh: GPIOH, 
+        gpioi: GPIOI, gpioj: GPIOJ, gpiok: GPIOK
+    ) -> Self {
+        let (mut disp_on, mut backlight) = init_pins(gpioe, gpiog, gpioh, gpioi, gpioj, gpiok);
+
+        let mut controller = DisplayController::new(
             ltdc,
             dma2d,
             PixelFormat::RGB565,
-            DISCO_SCREEN_CONFIG,
+            DISCO_CONFIG,
             Some(&HSEClock::new(25_000_000.Hz(), HSEClockMode::Bypass)),
         );
 
-        Stm32F7DiscoDisplay{controller}
+        controller.config_layer(
+            Layer::L1,
+            unsafe { &mut *core::ptr::addr_of_mut!(INTERNAL_FB) },
+            PixelFormat::RGB565,
+        );
+
+        controller.enable_layer(Layer::L1);
+        controller.reload();
+
+        disp_on.set_high();
+        backlight.set_high();
+
+        Self {
+            controller,
+            _disp_on: disp_on,
+            _backlight: backlight,
+        }
     }
 }
 
-impl DrawTarget for Stm32F7DiscoDisplay<u16> {
+impl DrawTarget for Display {
     type Color = Rgb565;
     type Error = core::convert::Infallible;
 
@@ -57,23 +89,67 @@ impl DrawTarget for Stm32F7DiscoDisplay<u16> {
         I: IntoIterator<Item = Pixel<Self::Color>>,
     {
         for Pixel(point, color) in pixels.into_iter() {
-            if point.x >= 0 && point.x < DISCO_SCREEN_CONFIG.active_width as i32 && point.y >= 0 && point.y < DISCO_SCREEN_CONFIG.active_height as i32 {
-                let raw_value = RawU16::from(color).into_inner();
-
-                self.controller.draw_pixel(
-                    Layer::L1, 
-                    point.x as usize, 
-                    point.y as usize, 
-                    raw_value
-                );
+            if point.x >= 0 && point.x < WIDTH as i32 && point.y >= 0 && point.y < HEIGHT as i32 {
+                let raw = RawU16::from(color).into_inner();
+                self.controller.draw_pixel(Layer::L1, point.x as usize, point.y as usize, raw);
             }
         }
         Ok(())
     }
 }
 
-impl OriginDimensions for Stm32F7DiscoDisplay<u16> {
+impl OriginDimensions for Display {
     fn size(&self) -> Size {
-        Size::new(DISCO_SCREEN_CONFIG.active_width as u32, DISCO_SCREEN_CONFIG.active_height as u32)
+        Size::new(WIDTH as u32, HEIGHT as u32)
     }
+}
+
+fn init_pins(
+    gpioe: GPIOE, gpiog: GPIOG, gpioh: GPIOH, 
+    gpioi: GPIOI, gpioj: GPIOJ, gpiok: GPIOK
+) -> (PI12<Output<PushPull>>, PK3<Output<PushPull>>) {
+    let gpioe = gpioe.split();
+    let gpiog = gpiog.split();
+    let gpioh = gpioh.split();
+    let gpioi = gpioi.split();
+    let gpioj = gpioj.split();
+    let gpiok = gpiok.split();
+
+    gpioe.pe4.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiog.pg12.into_alternate::<9>().set_speed(Speed::VeryHigh);
+    gpioi.pi9.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioi.pi10.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioi.pi13.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioi.pi14.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioi.pi15.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj0.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj1.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj2.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj3.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj4.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj5.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj6.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj7.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj8.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj9.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj10.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj11.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj13.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj14.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioj.pj15.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk0.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk1.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk2.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk4.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk5.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk6.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpiok.pk7.into_alternate::<14>().set_speed(Speed::VeryHigh);
+    gpioh.ph1.into_floating_input();
+
+    let mut disp_on = gpioi.pi12.into_push_pull_output();
+    disp_on.set_low();
+    let mut backlight = gpiok.pk3.into_push_pull_output();
+    backlight.set_high();
+
+    (disp_on, backlight)
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use defmt_rtt as _;
 use stm32f7xx_hal:: {
     pac,
     prelude::*,
-    gpio::Speed,
-    ltdc::{Layer, PixelFormat},
     rcc::{HSEClock, HSEClockMode}
 };
 
@@ -22,65 +20,12 @@ use embedded_graphics::{
     text::Text,
 };
 
-const WIDTH: u16 = 480;
-const HEIGHT: u16 = 272;
-
-const FB_GRAPHICS_SIZE: usize = (WIDTH as usize) * (HEIGHT as usize);
-static mut FB_LAYER1: [u16; FB_GRAPHICS_SIZE] = [0; FB_GRAPHICS_SIZE];
-
-
 #[entry]
 fn main() -> ! {
     defmt::println!("ZoomPunk booting up!");
     let device_peripherals = pac::Peripherals::take().unwrap();
     let core_peripherals = cortex_m::Peripherals::take().unwrap();
     let rcc = device_peripherals.RCC.constrain();
-
-    let _gpioa = device_peripherals.GPIOA.split();
-    let _gpiob = device_peripherals.GPIOB.split();
-    let gpioe = device_peripherals.GPIOE.split();
-    let gpiog = device_peripherals.GPIOG.split();
-    let gpioh = device_peripherals.GPIOH.split();
-    let gpioi = device_peripherals.GPIOI.split();
-    let gpioj = device_peripherals.GPIOJ.split();
-    let gpiok = device_peripherals.GPIOK.split();
-
-    gpioe.pe4.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_B0
-
-    gpiog.pg12.into_alternate::<9>().set_speed(Speed::VeryHigh); // LTCD_B4
-
-    gpioi.pi9.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_VSYNC
-    gpioi.pi10.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_HSYNC
-    gpioi.pi13.into_alternate::<14>().set_speed(Speed::VeryHigh);
-    gpioi.pi14.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_CLK
-    gpioi.pi15.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R0
-
-    gpioj.pj0.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R1
-    gpioj.pj1.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R2
-    gpioj.pj2.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R3
-    gpioj.pj3.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R4
-    gpioj.pj4.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R5
-    gpioj.pj5.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R6
-    gpioj.pj6.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_R7
-    gpioj.pj7.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G0
-    gpioj.pj8.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G1
-    gpioj.pj9.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G2
-    gpioj.pj10.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G3
-    gpioj.pj11.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G4
-    gpioj.pj13.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_B1
-    gpioj.pj14.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_B2
-    gpioj.pj15.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_B3
-
-    gpiok.pk0.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G5
-    gpiok.pk1.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G6
-    gpiok.pk2.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_G7
-    gpiok.pk4.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_B5
-    gpiok.pk5.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_B6
-    gpiok.pk6.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_D7
-    gpiok.pk7.into_alternate::<14>().set_speed(Speed::VeryHigh); // LTCD_E
-
-    gpioh.ph1.into_floating_input();
-
     let clocks = rcc
         .cfgr
         .hse(HSEClock::new(25_000_000.Hz(), HSEClockMode::Bypass))
@@ -88,26 +33,19 @@ fn main() -> ! {
         .hclk(216.MHz())
         .freeze();
 
-    let mut disp_on = gpioi.pi12.into_push_pull_output();
-    disp_on.set_low();
-
-    let mut backlight = gpiok.pk3.into_push_pull_output();
-    backlight.set_high();
-
-    let mut disp = display::Stm32F7DiscoDisplay::new(device_peripherals.LTDC, device_peripherals.DMA2D);
-    disp
-        .controller
-        .config_layer(Layer::L1, unsafe {&mut *core::ptr::addr_of_mut!(FB_LAYER1)}, PixelFormat::RGB565);
-
-    disp.controller.enable_layer(Layer::L1);
-    disp.controller.reload();
-
-    let disp = &mut disp;
-
-    disp_on.set_high();
+    let mut disp = display::Display::new(
+        device_peripherals.LTDC, 
+        device_peripherals.DMA2D,
+        device_peripherals.GPIOE,
+        device_peripherals.GPIOG,
+        device_peripherals.GPIOH,
+        device_peripherals.GPIOI,
+        device_peripherals.GPIOJ,
+        device_peripherals.GPIOK,
+    );
 
     let style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
-    Text::new("ZoomPunk is coming...", Point::new(20, 30), style).draw(disp).unwrap();
+    Text::new("ZoomPunk is coming...", Point::new(20, 30), style).draw(&mut disp).unwrap();
 
     let mut delay = Delay::new(core_peripherals.SYST, clocks.sysclk().to_Hz());
     loop {
